f683a188fd777fbb983547a10d8d6a9b51c47e54
Piotrekszmel <piotr.szmelczynski@gmail.com>
2023-10-21 01:42:09 +0200

    update echo in install_dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,7 +3,7 @@
 echo "Installing database dependencies..."
 pip install -r database/requirements.txt
 
-echo "Installing database dependencies..."
+echo "Installing development dependencies..."
 pip install -r requirements-dev.txt
 
 echo "All dependencies have been installed."
