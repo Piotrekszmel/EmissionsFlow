985aa92b8d4bfd6ebd921a20176b7b2b5a22d5ac
Piotrekszmel <piotr.szmelczynski@gmail.com>
2024-04-05 14:09:40 +0200

    remove schemas directory

--- database/schemas/001_create_invoices_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TYPE invoice_type AS ENUM ('Type1', 'Type2', 'Type3');
-
-CREATE TABLE Invoices (
-    id SERIAL PRIMARY KEY,
-    type invoice_type,
-    price FLOAT,
-    quantity FLOAT
-);
